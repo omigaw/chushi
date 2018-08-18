f673957d3038b75b9cd4633caf1dbc8a02941e61
Jessielovelife <xymlovelife@163.com>
2018-08-18 16:46:53 +0800

    del

--- backend/firstmeetschool/src/main/java/com/firstmeetschool/school/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.firstmeetschool.school.repository;
-
-import com.firstmeetschool.school.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
